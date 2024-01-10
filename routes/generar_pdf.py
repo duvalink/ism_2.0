42af977ddbfa3f945f778620e3c9e91d31da6707
duvalink <jose86mend@gmail.com>
2024-01-10 13:41:20 -0800

    Se habilito para el envio de correos

--- routes/generar_pdf.py
+++ routes/generar_pdf.py
@@ -243,7 +243,7 @@ class Pdf:
         # Ruta del archivo
         # ruta_pdf = os.path.join(os.getcwd(), "presupuestos_pdf", nombre_archivo)
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        ruta_pdf = os.path.join(dir_path, '..', 'presupuestos_pdf', nombre_archivo)
+        ruta_pdf = os.path.join(dir_path, "..", "presupuestos_pdf", nombre_archivo)
 
         # Verificar si el archivo original ya existe
         if os.path.exists(ruta_pdf):
@@ -252,13 +252,17 @@ class Pdf:
 
             # Adjuntar el consecutivo al nombre base, en caso de que ya exista el archivo pdf
             nuevo_nombre_archivo = f"P-{presupuesto_id}_corregido({contador}).pdf"
-            nueva_ruta_pdf = os.path.join(dir_path, '..', 'presupuestos_pdf', nuevo_nombre_archivo)
+            nueva_ruta_pdf = os.path.join(
+                dir_path, "..", "presupuestos_pdf", nuevo_nombre_archivo
+            )
 
             # Incrementar el contador hasta que se encuentre un nombre de archivo que no exista en el directorio
             while os.path.exists(nueva_ruta_pdf):
                 contador += 1
                 nuevo_nombre_archivo = f"P-{presupuesto_id}_({contador}).pdf"
-                nueva_ruta_pdf = os.path.join(dir_path, '..', 'presupuestos_pdf', nuevo_nombre_archivo)
+                nueva_ruta_pdf = os.path.join(
+                    dir_path, "..", "presupuestos_pdf", nuevo_nombre_archivo
+                )
 
             # Asignar el nuevo nombre al archivo
             nombre_archivo = nuevo_nombre_archivo
@@ -285,7 +289,6 @@ class Pdf:
 
         # Nombre base del archivo
         nombre_archivo, ruta_pdf = self.obtener_nombre_archivo_unico(presupuesto_id)
-    
 
         doc = SimpleDocTemplate(
             nombre_archivo,
@@ -423,7 +426,6 @@ class Pdf:
                     ("GRID", (0, 0), (-1, -1), 1, colors.black),
                     # NEGRITA
                     ("FONTNAME", (4, 1), (4, -1), "Helvetica-Bold"),
-                    
                 ]
             )
         )
@@ -597,11 +599,12 @@ class Pdf:
         with open(ruta_pdf, "rb") as f:
             pdf_data = f.read()
 
-        return redirect(url_for("cerrar"))
+        # return redirect(url_for("cerrar"))
 
-        # return redirect(
-        #     url_for("enviar_correo_presupuesto", presupuesto_id=presupuesto_id)
-        # )
+        # Habilitar solo cuando ya se este trabajando con los correos
+        return redirect(
+            url_for("enviar_correo_presupuesto", presupuesto_id=presupuesto_id)
+        )
 
     def rutas(self):
         self.app.add_url_rule(
